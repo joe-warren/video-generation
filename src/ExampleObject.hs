0a394b76bb790803f261a74eaf5d5c92875bdd3a
Joseph Warren <hungryjoe.warren@gmail.com>
2026-03-05 00:51:08 +0000

    More refactoring

--- src/ExampleObject.hs
+++ src/ExampleObject.hs
@@ -1,7 +1,7 @@
 module ExampleObject where
 
 import qualified Waterfall as W
-import Linear (V3 (..), (^*), unit, _x, _y )
+import Linear (V3 (..), (^*), unit, _x, _y)
 import Data.Function ((&))
 
 csgExample :: W.Solid
@@ -9,9 +9,9 @@ csgExample = let
     sphere = W.unitSphere
     sphere' = W.translate (unit _x) . W.uScale 0.1 $ sphere
     cube = W.uScale 1.5 W.centeredCube
-    cylinder = W.centeredCylinder
-         & W.scale (V3 0.55 0.55 4) 
+    cylinder = W.scale (V3 0.55 0.55 4) W.centeredCylinder
     cylinderA = W.rotate (unit _x) (pi/2) cylinder
     cylinderB = W.rotate (unit _y) (pi/2) cylinder
-    object = (cube `W.intersection` sphere) `W.difference` (cylinder `W.union` cylinderA `W.union` cylinderB)
+    object = (cube `W.intersection` sphere)
+         `W.difference` (cylinder <> cylinderA <> cylinderB)
   in object <> sphere'
