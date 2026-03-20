c6c658eb07f63feef468a7ef482920f6a732ee20
Joseph Warren <hungryjoe.warren@gmail.com>
2026-03-20 18:08:47 +0000

    Correct name of Melody and Rhythm

--- spatula/Audio.hs
+++ spatula/Audio.hs
@@ -9,13 +9,13 @@ module Audio where
 
 import Sound.Tidal.Boot
 
-introRhythm = cat 
+introMelody = cat 
     [ n "~  fs fs fs g   fs b  fs e@8"
     , n "~  e  e  e  cs6 as fs e  d@8" 
     , n "~  a  a  a  b   a  g  fs g@8" 
     , n "~  fs fs fs b   fs gs as b@8"
     ]
-introChords = cat
+introRhythm = cat
     [ n "b'minor   fs'major7"
     , n "fs'minor7 b'minor"
     , n "b'major7  e'minor"
@@ -27,8 +27,8 @@ introChords = cat
 addSwing = swingBy (1/7) 8
 
 intro = addSwing $ stack
-    [ introRhythm |- n 12 # sound "supersaw" # amp 0.8 
-    , rolled (introChords -| n "36" 
+    [ introMelody |- n 12 # sound "supersaw" # amp 0.8 
+    , rolled (introRhythm -| n "36" 
         # sound "supersquare" 
         # amp 0.3 
         # decay 0.0 
@@ -39,14 +39,14 @@ intro = addSwing $ stack
 
 -- BLOCK: Verse 1
 
-verse1Rhythm = cat
+verse1Melody = cat
     [ n "g  _  fs _  g  _  fs _  g  fs g  fs fs _  e  _" 
     , n "e  e  e  e  fs _  e  e  fs e  d  cs d  _  ~  ~"
     -- 
     , n "g  _  fs _  g  _  fs _  g  fs g  fs fs _  e  _"
     , n "b  b  a  g  g  _  fs _  fs fs gs as b  _  ~  ~"
     ]
-verse1Chords = cat 
+verse1Rhythm = cat 
     [ n "b'minor@3 fs'major7"
     , n "fs'major7@3 b'minor"
     , n "b'minor@2 b'major7 e'minor"
@@ -54,11 +54,11 @@ verse1Chords = cat
     ]
 
 -- BLOCK: Play Verse 1
-rhythmInstrument x = stack 
+melodyInstrument x = stack 
     [ x # amp 0.4 # sound "supersaw"
     , (x |- n "12") # amp 0.8 # sound "supersaw"
     ] # decay 0.25 # attack 0.05 # hold 0.2 # release 0.7 # legato 1.20
-chordInstrument x = rolled x -| n "36"
+rhythmInstrument x = rolled x -| n "48"
         # sound "superpwm" 
         # decay 0.5 # voice 0.5 # resonance 0.0 # lfo 0
         # dry 0.5 # room 0.5 # size 0.5
@@ -66,20 +66,20 @@ chordInstrument x = rolled x -| n "36"
         -- # attack 0.1 # hold 2.0 # release 1.0
 verse1 =
     addSwing $ stack
-    [ rhythmInstrument verse1Rhythm 
-    , chordInstrument verse1Chords
+    [ melodyInstrument verse1Melody 
+    , rhythmInstrument verse1Rhythm
     , s "<[sn sd:3] bd>*16" # amp 0.2 # pan "<0.3 0.6>*8"
     ] # cps (80/60/4)
 -- BLOCK: Verse 2
 
-verse2Rhythm = cat 
+verse2Melody = cat 
     [ n "b  _  b  _  a  _  g  _  b  b  a  g  b  b  a  g"
     , n "fs fs fs fs e  _  fs _  fs e  d  cs b  _  ~  ~"
     --- 
     , n "b  _  b  _  a  _  g  _  b  b  a  g  b  b  a  g"
     , n "fs fs fs fs fs fs fs fs fs _  gs as b  _  ~  ~"
     ]
-verse2Chords = cat 
+verse2Rhythm = cat 
     [ n "e'minor b'major7"
     , n "fs'major7@2 fs'major7 b'major7"
     , n "e'minor b'major7"
@@ -96,8 +96,8 @@ varyTempo inn mid out =
 -- BLOCK: Play Verse 2
 verse2 = 
     addSwing $ stack
-    [ rhythmInstrument verse2Rhythm
-    , chordInstrument verse2Chords 
+    [ melodyInstrument verse2Melody
+    , rhythmInstrument verse2Rhythm 
     , s "<[sn:2 bd] [sd:3]>*16" # amp 0.2 # pan "<0.3 0.6>*8"
     ] # cps (80/60/4)
     
